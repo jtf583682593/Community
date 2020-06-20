f3852b4f2ad029686bdfbd2f042d95a346b2227b
jtf <jiangtf@soyuan.com.cn>
2020-06-21 00:07:05 +0800

    删除无用HTML

--- src/main/java/jtf/community/community/contorller/AuthorizeCotroller.java
+++ src/main/java/jtf/community/community/contorller/AuthorizeCotroller.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeCotroller {
     @Autowired
@@ -22,7 +24,8 @@ public class AuthorizeCotroller {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO=new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientid);
         accessTokenDTO.setClient_secret(secret);
@@ -31,7 +34,11 @@ public class AuthorizeCotroller {
         accessTokenDTO.setState(state);
         String accessToken=githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user=githubProvider.gitUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if(user!=null){
+            request.getSession().setAttribute("user",user);
+            return "redirect:index";
+        }else {
+            return "redirect:index";
+        }
     }
 }
